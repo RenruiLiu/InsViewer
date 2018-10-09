8523901f42cd13be1a1057411932f911cba5aed6
Ryan 瑞兽瑞兽 <liur@deakin.edu.au>
2018-10-09 22:51:32 +1100

    load location in UI
    next:
    1. store location alone with post in firebase
    2. modify post with new para location
    3. fetch location and display on UI (probably right side of likes)

--- InsViewer/AddPhoto/SharePhotoViewController.swift
+++ InsViewer/AddPhoto/SharePhotoViewController.swift
@@ -8,9 +8,12 @@
 
 import UIKit
 import Firebase
+import CoreLocation
 
-class SharePhotoViewController: UIViewController {
+class SharePhotoViewController: UIViewController, CLLocationManagerDelegate {
     
+    var locationManager = CLLocationManager()
+
     static let updateFeedNotificationName = NSNotification.Name(rawValue: "UpdateFeed")
     
     var selectedImg: UIImage?{
@@ -32,6 +35,26 @@ class SharePhotoViewController: UIViewController {
         return tv
     }()
     
+    let showLocationLabel: UILabel = {
+        let lb = UILabel()
+        lb.text = "Location: "
+        lb.font = UIFont.systemFont(ofSize: 14)
+        lb.textAlignment = .center
+        return lb
+    }()
+    lazy var addLocationSwitch: UISwitch = {
+        let sw = UISwitch()
+        sw.addTarget(self, action: #selector(handleSwitchValueChanged), for: .valueChanged)
+        return sw
+    }()
+    let locationLabel: UILabel = {
+        let lb = UILabel()
+        lb.text = ""
+        lb.numberOfLines = 2
+        lb.font = UIFont.systemFont(ofSize: 12)
+        return lb
+    }()
+    
     fileprivate func setupImageAndTextViews(){
         let containerView = UIView()
         containerView.backgroundColor = .white
@@ -44,6 +67,14 @@ class SharePhotoViewController: UIViewController {
         //
         containerView.addSubview(textView)
         textView.anchor(top: containerView.topAnchor, paddingTop: 0, bottom: containerView.bottomAnchor, paddingBottom: 0, left: imgView.rightAnchor, paddingLeft: 4, right: containerView.rightAnchor, paddingRight: 0, width: 0, height: 0)
+        
+        
+        view.addSubview(addLocationSwitch)
+        addLocationSwitch.anchor(top: containerView.bottomAnchor, paddingTop: 8, bottom: nil, paddingBottom: 0, left: containerView.leftAnchor, paddingLeft: 16, right: nil, paddingRight: 0, width: 0, height: 30)
+        view.addSubview(showLocationLabel)
+        showLocationLabel.anchor(top: containerView.bottomAnchor, paddingTop: 8, bottom: nil, paddingBottom: 0, left: addLocationSwitch.rightAnchor, paddingLeft: 4, right: nil, paddingRight: 0, width: 0, height: 30)
+        view.addSubview(locationLabel)
+        locationLabel.anchor(top: containerView.bottomAnchor, paddingTop: 8, bottom: nil, paddingBottom: 0, left: showLocationLabel.rightAnchor, paddingLeft: 4, right: containerView.rightAnchor, paddingRight: 4, width: 0, height: 30)
     }
     
     //____________________________________________________________________________________
@@ -97,7 +128,40 @@ class SharePhotoViewController: UIViewController {
             self.dismiss(animated: true, completion: nil)
         }
     }
-
+    
+    var isShowingLocation: Bool = false
+    @objc fileprivate func handleSwitchValueChanged(){
+        isShowingLocation = !isShowingLocation
+        if isShowingLocation == false {
+            locationLabel.text = ""
+            return
+        }
+        locationLabel.text = "loading..."
+        
+        locationManager.startUpdatingLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+            CLGeocoder().reverseGeocodeLocation(manager.location!) { (placemarks, err) in
+            if (err != nil) {
+                print("Failed to reverse geocoder:",err!)
+                return
+            }
+            if placemarks?.count ?? 0 > 0 && self.isShowingLocation {
+                let pm = placemarks?[0] as! CLPlacemark
+                let address = pm.name ?? ""
+                let suburb = pm.locality ?? ""
+                let city = pm.subLocality ?? ""
+                self.locationLabel.text = "\(address) \(suburb) \(city) "
+                manager.stopUpdatingLocation()
+            }
+        }
+    }
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Failed to fetch user location:",error.localizedDescription)
+    }
+    
+    
     //____________________________________________________________________________________
     override var prefersStatusBarHidden: Bool {return true}
     override func viewDidLoad() {
@@ -106,6 +170,13 @@ class SharePhotoViewController: UIViewController {
         view.backgroundColor = UIColor.rgb(red: 240, green: 240, blue: 240)
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: NSLocalizedString("share", comment: ""), style: .plain, target: self, action: #selector(handleShare))
         setupImageAndTextViews()
+        
+        self.locationManager.requestWhenInUseAuthorization()
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+            locationManager.startUpdatingLocation()
+        }
     }
 
 }
